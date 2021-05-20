2678d64b3165371fb80a5056bb6074d3ae296634
kys0119 <sunnkys119@naver.com>
2021-05-20 15:00:26 +0900

    0520 프로젝트 추가

---
+++ 0520/src/TestExam.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class TestExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new scan(scan.in);
+		
+		System.out.println("500 이하의 자연수를 입력하세요.");
+		
+		int sum=0;
+		number
+		
+		for(i=1; i<number; i++);
+			if(i%3 && );
+	}
+	scan.close;
+}
